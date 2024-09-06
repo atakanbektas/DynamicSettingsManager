e507e26fa3119775c63cda34523c6bb2d59e89df
Atakan <123401930+atakanbektas@users.noreply.github.com>
2024-09-06 21:48:04 +0300

    IAppConfigService and IUnitOfWork created.

---
+++ DynamicSettingsManagerApp/SettingsManagerApp.Application/IAppConfigService.cs
@@ -0,0 +1,19 @@
+﻿using SettingManagerApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsManagerApp.Application
+{
+    public interface IAppConfigService
+    {
+        Task<bool> AddAppConfigAsync(AppConfiguration appConfiguration);
+        Task<bool> GetAppConfigAsync(AppConfiguration appConfiguration);
+        Task<bool> DeleteAppConfigAsync(AppConfiguration appConfiguration);
+        Task<bool> UpdateAppConfigAsync(int id, AppConfiguration appConfiguration);
+        Task<AppConfiguration> GetAppConfigByNameAsync(string name);
+        Task<IEnumerable<AppConfiguration>> GetAppConfigsAsync();
+    }
+}
---
+++ DynamicSettingsManagerApp/SettingsManagerApp.Application/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using SettingsManagerApp.Application.Repositories.AppConfigRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsManagerApp.Application
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAppConfigReadRepository AppConfigRead { get; }
+        IAppConfigWriteRepository AppConfigWrite { get; }
+    }
+}
